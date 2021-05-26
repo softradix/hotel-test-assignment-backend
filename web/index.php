954b06b2521d80793cfaca404b585d7223fd199b
softradix <github@softradix.in>
2021-05-26 01:33:25 -0400

    Cors issue

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, accept, authorization, Content-Type, X-Auth-Tokens, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
